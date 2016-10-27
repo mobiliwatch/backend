d4a86c61bb7660d496aa21ce820a7f4e1000fc8e
Bastien Abadie <bastien@nextcairn.com>
2016-10-27 11:38:45 +0200

    Simple signup

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth
+from users.views import Signup
 
 urlpatterns = (
+    url('^signup/$', Signup.as_view(), name='signup'),
     url('^login/$', auth.login, name='login'),
     url('^logout/$', auth.logout, name='logout'),
 )
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse
+from django.contrib.auth import login
+from users.forms import UserCreationForm
+from users.models import User
 
-# Create your views here.
+class Signup(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        # TODO: redirect to location creator
+        return reverse('home')
+
+    def form_valid(self, form):
+        # Create user & build redirect
+        out = super(Signup, self).form_valid(form)
+
+        # Login user
+        login(self.request, self.object)
+
+        return out
