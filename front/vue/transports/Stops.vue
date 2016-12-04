e4ebf48e8b51eb6f5cfd652e85ebab433e732110
Bastien Abadie <bastien@nextcairn.com>
2016-12-04 21:34:20 +0100

    wip stops  nice

--- front/vue/transports/Stops.vue
+++ front/vue/transports/Stops.vue
@@ -20,9 +20,21 @@ module.exports = {
         stop.line_stops.forEach(function(ls){
           lines.push(ls.line);
         });
-        out[stop.id] = _.uniqBy(lines, function(l){
+
+        // Unique sorted lines
+        lines = _.sortBy(lines, function(l){
+          return l.name;
+        });
+        lines = _.sortedUniqBy(lines, function(l){
           return l.id;
         });
+
+        // Group by mode
+        lines = _.groupBy(lines, function(l){
+          return l.mode;
+        });
+
+        out[stop.id] = lines;
       });
       return out;
     },
@@ -44,50 +56,55 @@ module.exports = {
 
 <template>
   <aside class="menu">
-    <ul class="menu-list">
-      <li v-on:click="selected(stop)" v-for="(stop, i) in stops">
-        <span class="name" v-bind:class="{'is-active' : stop == current_stop}">
+    <ul>
+      <li v-on:click="selected(stop)" v-for="(stop, i) in stops" v-bind:class="{'is-active' : stop == current_stop}">
+        <p class="name">
           <strong>{{ stop.name }}</strong> à 
           <em v-if="!stop.trip">{{ stop.approximate_distance }} m</em>
           <span v-if="stop.trip">{{ stop.trip.distance }} m - {{ stop.trip.duration|seconds }}</span>
-        </span>
-        <ul>
-<span v-for="line in lines[stop.id]" class="tag" :class="{'is-success' : line.mode == 'tram', 'is-primary' : line.mode == 'bus', 'is-info' : line.mode == 'car'}">{{ line.name }}</span>
-        </ul>
+        </p>
+        <p class="lines">
+          <p v-for="(mode_lines,mode) in lines[stop.id]">
+            <span class="icon">
+              <span class="fa fa-train" title="Tramway" v-if="mode == 'tram'"></span>
+              <span class="fa fa-bus" title="Bus" v-if="mode == 'bus'"></span>
+              <span class="fa fa-bus" title="Autocar" v-if="mode == 'car'"></span>
+            </span>
+            <span v-for="line in mode_lines" class="tag" :class="{'is-success' : line.mode == 'tram', 'is-primary' : line.mode == 'bus', 'is-info' : line.mode == 'car'}">{{ line.name }}</span>
+          </p>
+        </p>
       </li>
     </ul>
   </aside>
 </template>
 
 <style scoped>
-ul.menu-list li {
+ul li {
   cursor: pointer;
   margin-bottom: 6px;
-}
-
-ul.menu-list li ul {
-  margin-top: 2px !important;
-  margin-bottom: 2px !important;
-}
-
-ul.menu-list span.name {
-  display: block;
-  padding: 0 5px;
   border-radius: 3px 3px;
+  padding: 3px;
 }
 
-ul.menu-list span.is-active {
-  background: #3273dc;
-  color: white;
+ul li.is-active {
+  background: #EEE;
 }
 
-ul.menu-list span.is-active strong {
-  color: white;
+ul li p {
+  margin-bottom: 4px;
 }
 
-p.no-selection {
-  font-size: 0.8em;
-  margin-left: 8px;
-  color: #CCC;
+ul li p.name * {
+  font-size: 1.2em !important;
 }
+
+ul li span.icon {
+  color: #888;
+}
+
+ul li p span.tag {
+  margin-right: 2px;
+  margin-bottom: 2px;
+}
+
 </style>
